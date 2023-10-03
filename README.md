64e6d8705106b1536fea61b063f015e9450f5881
Juan Garcia <garciaperezjuandedios2001@gmail.com>
2023-10-02 22:08:49 -0600

    seccion 13

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # WebServer + RestServer
 
 Recuerden que deben de ejecutar ```npm install``` para reconstruir los módulos de Node.
-Actualizacion seccion 12
+Actualizacion seccion 12 productos
--- models/server.js
+++ models/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const { dbConnection } = require('../database/config');
+
 
 class Server {
 
@@ -15,6 +17,7 @@ class Server {
             categorias: '/api/categorias',
             productos: '/api/productos',
             usuarios:   '/api/usuarios',
+            upload:   '/api/upload',
 
         }
     
@@ -45,6 +48,12 @@ class Server {
         // Directorio Público
         this.app.use( express.static('public') );
 
+        //FIle Upload
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        }));
     }
 
     routes() {
@@ -54,6 +63,7 @@ class Server {
         this.app.use( this.paths.categorias, require('../routes/categorias'));
         this.app.use( this.paths.productos, require('../routes/productos'));
         this.app.use( this.paths.usuarios, require('../routes/usuarios'));
+        this.app.use( this.paths.upload, require('../routes/upload'));
        
     }
 
